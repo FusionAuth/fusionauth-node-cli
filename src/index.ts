e10f77386d3b5f1a61726bd49ee11f19d24781a6
Colin Frick <colin@frick.io>
2023-07-31 12:27:37 +0200

    feat: automatically pull CLI version from package.json
    Closes #6

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-#! /usr/bin/env node
+#! /usr/bin/env -S node --no-warnings=ExperimentalWarning
 
 import figlet from 'figlet';
 import {Command} from 'commander';
 import chalk from 'chalk';
 import * as commands from './commands/index.js';
+// @ts-ignore
+import pkg from '../package.json' assert { type: 'json' };
 
 const fusionString = figlet.textSync('Fusion').split('\n');
 const authString = figlet.textSync('Auth').split('\n');
@@ -16,7 +18,7 @@ const program = new Command();
 program
     .name('@fusionauth/cli')
     .description('CLI for FusionAuth')
-    .version('1.1.2');
+    .version(pkg.version);
 
 Object.values(commands).forEach(command => program.addCommand(command));
 
