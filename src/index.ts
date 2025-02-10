d80d22cef4d3e2a19bf1e4308a23cb126bd0ebf7
Alex Patterson <139158618+alex-fusionauth@users.noreply.github.com>
2025-02-10 08:19:03 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,15 @@
 #! /usr/bin/env -S node --no-warnings=ExperimentalWarning
-
-import figlet from 'figlet';
-import {Command} from '@commander-js/extra-typings';
+import * as fs from 'node:fs';
+import { Command } from '@commander-js/extra-typings';
 import chalk from 'chalk';
+import figlet from 'figlet';
 import * as commands from './commands/index.js';
-// @ts-ignore
-import pkg from '../package.json' with { type: 'json' };
-
 const fusionString = figlet.textSync('Fusion').split('\n');
 const authString = figlet.textSync('Auth').split('\n');
-
 fusionString.forEach((line, i) => {
-    console.log(chalk.white(line) + chalk.hex('#F58320')(authString[i]));
+  console.log(chalk.white(line) + chalk.hex('#F58320')(authString[i]));
 });
-
 const program = new Command();
-program
-    .name('@fusionauth/cli')
-    .description('CLI for FusionAuth')
-    .version(pkg.version);
-
-Object.values(commands).forEach(command => program.addCommand(command));
-
+program.name('@fusionauth/cli').description('CLI for FusionAuth');
+Object.values(commands).forEach((command) => program.addCommand(command));
 program.parse();
